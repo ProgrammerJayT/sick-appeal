5d39000d8f2ede183611f041bb38eacdcf10724a
ProgrammerJayT <theanthem8@gmail.com>
2023-03-10 17:42:43 +0200

    Disabled lecturer for time-being

--- api/database/migrations/2023_03_10_082812_create_lecturers_table.php
+++ api/database/migrations/2023_03_10_082812_create_lecturers_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('lecturers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        // Schema::create('lecturers', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
     }
 
     /**
