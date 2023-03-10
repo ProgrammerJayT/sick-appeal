d1b1fca35773191b382cc53d8ee6f4355ce4a4ff
ProgrammerJayT <theanthem8@gmail.com>
2023-03-10 11:32:33 +0200

    Added accounts table attributes

--- api/database/migrations/2023_03_10_082828_create_accounts_table.php
+++ api/database/migrations/2023_03_10_082828_create_accounts_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('accounts', function (Blueprint $table) {
-            $table->id();
+            $table->integer('account_id')->autoIncrement();
+            $table->string('email');
+            $table->string('password');
+            $table->string('role');
             $table->timestamps();
         });
     }
