c3a01840917f361db26753ecd2f9dbc8f48c43ce
ProgrammerJayT <theanthem8@gmail.com>
2023-03-11 08:49:14 +0200

    Added user-defined primary key

--- api/app/Models/Student.php
+++ api/app/Models/Student.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Account;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
+    protected $primaryKey = 'student_id';
+    protected $guarded = [];
     use HasFactory;
+
+    public function account()
+    {
+        return $this->belongsTo(Account::class, 'account_id');
+    }
 }
