a4e1c99ef1df275e2669a70f8cc268ec206d2286
ProgrammerJayT <theanthem8@gmail.com>
2023-03-12 17:18:16 +0200

    Added attributes for factorization

--- api/database/factories/StudentFactory.php
+++ api/database/factories/StudentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Account;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +17,17 @@ class StudentFactory extends Factory
      */
     public function definition(): array
     {
+
+        //Get last 2 digits of the year, and convert to int
+        $studentNumberMin = (int) substr(date('Y'), -2) + 229999977;
+        $studentNumberMax = (int) substr(date('Y'), -2) + 239999976;
+
         return [
             //
+            'student_id' => $this->faker->unique()->numberBetween($studentNumberMin, $studentNumberMax),
+            'account_id' => Account::factory(),
+            'name' => $this->faker->firstName(),
+            'surname' => $this->faker->lastName(),
         ];
     }
 }
