acdc05d68c7ca389b1a5800fbc40414c8187e5ef
ProgrammerJayT <theanthem8@gmail.com>
2023-03-12 21:37:57 +0200

    Called unique student number dummy generator

--- api/database/factories/StudentFactory.php
+++ api/database/factories/StudentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Http\Controllers\Generator;
 use App\Models\Account;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -17,14 +18,12 @@ class StudentFactory extends Factory
      */
     public function definition(): array
     {
-
-        //Get last 2 digits of the year, and convert to int
-        $studentNumberMin = (int) substr(date('Y'), -2) + 229999977;
-        $studentNumberMax = (int) substr(date('Y'), -2) + 239999976;
+        $generator = new Generator;
+        $identity = $generator->generateIdentity();
 
         return [
             //
-            'student_id' => $this->faker->unique()->numberBetween($studentNumberMin, $studentNumberMax),
+            'student_id' => $identity,
             'account_id' => Account::factory(),
             'name' => $this->faker->firstName(),
             'surname' => $this->faker->lastName(),
