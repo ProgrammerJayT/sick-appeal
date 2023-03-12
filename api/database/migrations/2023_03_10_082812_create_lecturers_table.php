731bca66c595f1f10bfb644ba82dbe6867d4e468
ProgrammerJayT <theanthem8@gmail.com>
2023-03-12 17:18:43 +0200

    Added table attributes

--- api/database/migrations/2023_03_10_082812_create_lecturers_table.php
+++ api/database/migrations/2023_03_10_082812_create_lecturers_table.php
@@ -4,17 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        // Schema::create('lecturers', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->timestamps();
-        // });
+        Schema::create('lecturers', function (Blueprint $table) {
+            $table->integer('lecturer_id')->autoIncrement();
+            $table->integer('account_id');
+            $table->string('modules')->nullable();
+            $table->string('name');
+            $table->string('surname');
+            $table->timestamps();
+        });
     }
 
     /**
