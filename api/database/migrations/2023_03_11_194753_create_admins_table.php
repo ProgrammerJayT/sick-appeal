e5a7cc53692d40f654dddd9dd750ec8544877188
ProgrammerJayT <theanthem8@gmail.com>
2023-03-19 17:22:26 +0200

    Added admin table attributes

--- api/database/migrations/2023_03_11_194753_create_admins_table.php
+++ api/database/migrations/2023_03_11_194753_create_admins_table.php
@@ -4,15 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
+            $table->integer('admin_id')->autoIncrement();
+            $table->integer('account_id');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email');
             $table->timestamps();
         });
     }
