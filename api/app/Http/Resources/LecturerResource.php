bd7ae68b6b51450258f2f1c54beea4a4ca859e10
ProgrammerJayT <theanthem8@gmail.com>
2023-03-19 18:25:21 +0200

    Added json response request properties

--- api/app/Http/Resources/LecturerResource.php
+++ api/app/Http/Resources/LecturerResource.php
@@ -14,6 +14,12 @@ class LecturerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return array(
+            'lecturerId' => $this->lecturer_id,
+            'accountId' => $this->account_id,
+            'name' => $this->name,
+            'surname' => $this->surname,
+            'email' => $this->email,
+        );
     }
 }
