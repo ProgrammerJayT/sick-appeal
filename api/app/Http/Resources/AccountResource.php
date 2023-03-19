b98641cbc58d1fab95cba6ec5c816368375d2c39
ProgrammerJayT <theanthem8@gmail.com>
2023-03-19 17:02:10 +0200

    Added email properties to json response requests

--- api/app/Http/Resources/AccountResource.php
+++ api/app/Http/Resources/AccountResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Admin;
+use App\Models\Student;
+use App\Models\Lecturer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,8 +19,34 @@ class AccountResource extends JsonResource
     {
         return array(
             'accountId' => $this->account_id,
+            'type' => $this->type,
+            'status' => $this->status,
             'email' => $this->email,
-            'role' => $this->role,
+            'emailVerified' => $this->email_verified,
+            'user' => $this->getUser($this->type, $this->account_id),
         );
+    }
+
+    public function getUser($type, $accountId)
+    {
+        switch ($type) {
+            case 'student':
+                $user = new StudentResource(Student::where('account_id', $accountId)->first());
+                break;
+
+            case 'admin':
+                // $user = new AdminResource(Admin::where('account_id', $accountId)->first());
+                break;
+
+            case 'lecturer':
+                $user = new LecturerResource(Lecturer::where('account_id', $accountId)->first());
+                break;
+
+            default:
+                # code...
+                break;
+        }
+
+        return $user;
     }
 }
