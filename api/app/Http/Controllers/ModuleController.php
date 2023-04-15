3bf19f120527d8d32fe480895a5a2f12d17c4908
ProgrammerJayT <theanthem8@gmail.com>
2023-04-16 00:05:22 +0200

    Implemented module store

--- api/app/Http/Controllers/ModuleController.php
+++ api/app/Http/Controllers/ModuleController.php
@@ -8,6 +8,8 @@ use App\Http\Requests\StoreModuleRequest;
 use App\Http\Requests\UpdateModuleRequest;
 use App\Http\Resources\ModuleCollection;
 use App\Http\Resources\ModuleResource;
+use App\Models\Course;
+use App\Models\CourseModule;
 use Illuminate\Http\Request;
 
 class ModuleController extends Controller
@@ -29,7 +31,46 @@ class ModuleController extends Controller
      */
     public function store(StoreModuleRequest $request)
     {
-        //
+        // Get the course name from the request
+        $name = $request->name;
+        $code = $request->code;
+        $course_id = $request->courseId;
+
+        //First check if the course exists
+        $existingCourse = Course::find($course_id);
+        if (!$existingCourse) {
+            return response()->json(['error' => 'Course not found'], 404);
+        }
+
+        // Check if the module already exists in the database
+        $existingModule = Module::where('name', $name)->where('code', $code)->first();
+
+        if (!$existingModule) {
+            //Since the module doesn't exist, create new one
+            $newModule = Module::create([
+                'name' => $name,
+                'code' => $code
+            ]);
+
+            $module = $newModule;
+
+        } else {
+            //The module exists, it can be shared, but is it already placed under the requested course?
+            $existingCourseModule = CourseModule::where('course_id', $course_id)->where('module_id', $existingModule->module_id)->first();
+
+            //If module already has a course, return error
+            if ($existingCourseModule != null) {
+                return response()->json(['error' => 'Module already exists'], 409);
+            }
+        }
+
+        //After creating a new module, add new record to course module entity
+        CourseModule::create([
+            'course_id' => $course_id,
+            'module_id' => $module ?? $existingModule->module_id
+        ]);
+
+        return new ModuleResource($module ?? $existingModule);
     }
 
     /**
