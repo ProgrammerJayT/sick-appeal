1f0e5f99d874472fb8e6239023848f769879d0ae
ProgrammerJayT <theanthem8@gmail.com>
2023-04-15 14:29:49 +0200

    Added attributes

--- api/database/migrations/2023_04_15_122718_create_tests_table.php
+++ api/database/migrations/2023_04_15_122718_create_tests_table.php
@@ -4,15 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('tests', function (Blueprint $table) {
-            $table->id();
+            $table->integer('test_id')->autoIncrement();
+            $table->integer('lecturer_id');
+            $table->integer('course_module_id');
+            $table->date('date');
+            $table->time('time');
+            $table->string('type');
             $table->timestamps();
         });
     }
