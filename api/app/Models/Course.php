eab30d54be7f6efe4b9cf6112ef2397351e5654d
ProgrammerJayT <theanthem8@gmail.com>
2023-04-15 23:58:18 +0200

    Added relationships

--- api/app/Models/Course.php
+++ api/app/Models/Course.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Module;
+use App\Models\CourseModule;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
     protected $primaryKey = 'course_id';
     protected $guarded = [];
     use HasFactory;
+
+    public function courseModules()
+    {
+        return $this->hasMany(CourseModule::class);
+    }
+
+    public function modules()
+    {
+        return $this->belongsToMany(Module::class, 'course_modules');
+    }
 }
--- api/app/Models/CourseModule.php
+++ api/app/Models/CourseModule.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Course;
+use App\Models\Module;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CourseModule extends Model
 {
     protected $primaryKey = 'course_module_id';
     protected $guarded = [];
     use HasFactory;
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
 }
