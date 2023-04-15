a9f79c80b9a19274788ae8019dcb2896d1af27da
ProgrammerJayT <theanthem8@gmail.com>
2023-04-15 12:09:16 +0200

    Added attributes and constraints

--- api/database/migrations/2023_04_15_094829_create_course_modules_table.php
+++ api/database/migrations/2023_04_15_094829_create_course_modules_table.php
@@ -4,15 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('course_modules', function (Blueprint $table) {
-            $table->id();
+            $table->integer('course_module_id')->primary();
+            $table->integer('course_id');
+            $table->integer('module_id');
+
+            //Table constraints
+            $table->foreign('course_id')->references('course_id')->on('courses')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('module_id')->references('module_id')->on('modules')->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
         });
     }
