027859cadb9a74f377f3e39156a17d446e327653
ProgrammerJayT <theanthem8@gmail.com>
2023-04-15 23:57:20 +0200

    Added course module functionality

--- api/database/seeders/CourseModuleSeeder.php
+++ api/database/seeders/CourseModuleSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
+use App\Models\CourseModule;
+use App\Models\Module;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +16,18 @@ class CourseModuleSeeder extends Seeder
     public function run(): void
     {
         //
+        $modules = Module::all();
+        $courses = Course::all();
+
+        for ($i = 0; $i < count($courses); $i++) {
+            $numCourses = rand(0, count($modules));
+
+            for ($j = 0; $j < $numCourses; $j++) {
+                CourseModule::create([
+                    'course_id' => $courses->random()->getKey(),
+                    'module_id' => $modules->random()->getKey()
+                ]);
+            }
+        }
     }
 }
