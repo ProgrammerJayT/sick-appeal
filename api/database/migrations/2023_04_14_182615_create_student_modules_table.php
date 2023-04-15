2ef89a97c153073d8a5e0f12fc117f2c00ef63b4
ProgrammerJayT <theanthem8@gmail.com>
2023-04-15 14:18:58 +0200

    Added attributes

--- api/database/migrations/2023_04_14_182615_create_student_modules_table.php
+++ api/database/migrations/2023_04_14_182615_create_student_modules_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('student_modules', function (Blueprint $table) {
-            $table->id();
+            $table->integer('student_module_id')->autoIncrement();
+            $table->integer('course_module_id');
+            $table->integer('student_id');
             $table->timestamps();
         });
     }
