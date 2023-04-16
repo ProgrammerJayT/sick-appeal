73d927593b1bf7a733b5cc0966c4fd0602eb5ab6
ProgrammerJayT <theanthem8@gmail.com>
2023-04-16 10:05:02 +0200

    Created test resource

---
+++ api/app/Http/Resources/TestResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return array(
+            'testId' => $this->test_id,
+            'lecturerId' => $this->lecturer_id,
+            'courseModuleId' => $this->course_module_id,
+            'date' => $this->date,
+            'time' => $this->time,
+            'type' => $this->type
+        );
+    }
+}
