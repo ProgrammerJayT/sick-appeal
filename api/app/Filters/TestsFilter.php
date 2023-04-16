2ffc6141cf05f3994187b06999e6573ece90d74b
ProgrammerJayT <theanthem8@gmail.com>
2023-04-16 10:39:05 +0200

    Created tests filter

---
+++ api/app/Filters/TestsFilter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filters;
+
+use App\Filters\ApiFilter;
+
+class TestsFilter extends ApiFilter
+{
+    protected $safeParams = array(
+        'lecturerId' => array(
+            'eq',
+        ),
+        'courseModuleId' => array(
+            'eq',
+        ),
+        'date' => array(
+            'eq',
+            'ne',
+            'gt',
+            'lt',
+            'gte',
+            'lte'
+        ),
+        'time' => array(
+            'eq',
+            'ne',
+            'gt',
+            'lt',
+            'gte',
+            'lte'
+        ),
+        'type' => array(
+            'eq',
+            'like',
+            'ne'
+        ),
+    );
+
+    // For camel cased params
+    protected $columnMap = array(
+        'courseModuleId' => 'course_module_id',
+    );
+}
