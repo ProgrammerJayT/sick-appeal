f059ff068fb6f5f0c4c55d4f761b9db164785d49
ProgrammerJayT <theanthem8@gmail.com>
2023-04-16 10:01:59 +0200

    Implemented destroy method

--- api/app/Http/Controllers/TestController.php
+++ api/app/Http/Controllers/TestController.php
@@ -17,14 +17,6 @@ class TestController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTestRequest $request)
@@ -38,14 +30,7 @@ class TestController extends Controller
     public function show(Test $test)
     {
         //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Test $test)
-    {
-        //
+        // return new TestRes
     }
 
     /**
@@ -62,5 +47,6 @@ class TestController extends Controller
     public function destroy(Test $test)
     {
         //
+        return $test->delete();
     }
 }
