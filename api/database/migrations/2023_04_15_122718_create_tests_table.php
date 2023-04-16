716ef5c9ede81682c5bb98c69ddab91d57c6c234
ProgrammerJayT <theanthem8@gmail.com>
2023-04-16 11:55:35 +0200

    Added status attribute

--- api/database/migrations/2023_04_15_122718_create_tests_table.php
+++ api/database/migrations/2023_04_15_122718_create_tests_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->date('date');
             $table->time('time');
             $table->string('type');
+            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
