251346d5725c3ce2e84c8b0dc22de9e66fea9861
ProgrammerJayT <theanthem8@gmail.com>
2023-04-21 18:52:36 +0200

    Created student application entity

---
+++ api/app/Http/Controllers/StudentApplicationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentApplication;
+use App\Http\Requests\StoreStudentApplicationRequest;
+use App\Http\Requests\UpdateStudentApplicationRequest;
+
+class StudentApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudentApplicationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentApplication $studentApplication)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentApplication $studentApplication)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudentApplicationRequest $request, StudentApplication $studentApplication)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentApplication $studentApplication)
+    {
+        //
+    }
+}
