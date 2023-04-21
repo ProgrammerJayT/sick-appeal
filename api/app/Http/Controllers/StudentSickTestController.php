b4cf096e4d25312f3fc7d1f15db6860443aa02e0
ProgrammerJayT <theanthem8@gmail.com>
2023-04-21 18:54:35 +0200

    Created student sick test entity

---
+++ api/app/Http/Controllers/StudentSickTestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentSickTest;
+use App\Http\Requests\StoreStudentSickTestRequest;
+use App\Http\Requests\UpdateStudentSickTestRequest;
+
+class StudentSickTestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudentSickTestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentSickTest $studentSickTest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentSickTest $studentSickTest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudentSickTestRequest $request, StudentSickTest $studentSickTest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentSickTest $studentSickTest)
+    {
+        //
+    }
+}
