78c0ab492e06400d233ae33c2110bf59819bf356
ProgrammerJayT <theanthem8@gmail.com>
2023-04-21 18:50:13 +0200

    Created sick test entity

---
+++ api/app/Http/Controllers/SickTestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SickTest;
+use App\Http\Requests\StoreSickTestRequest;
+use App\Http\Requests\UpdateSickTestRequest;
+
+class SickTestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSickTestRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SickTest $sickTest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SickTest $sickTest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSickTestRequest $request, SickTest $sickTest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SickTest $sickTest)
+    {
+        //
+    }
+}
