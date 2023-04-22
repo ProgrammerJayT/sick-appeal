09ef3cf9492badf00d2d7af1a331c3c962bab03a
ProgrammerJayT <theanthem8@gmail.com>
2023-04-22 20:48:41 +0200

    Implemented registration seeder

--- api/database/seeders/RegistrationSeeder.php
+++ api/database/seeders/RegistrationSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
+use App\Models\Registration;
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +16,13 @@ class RegistrationSeeder extends Seeder
     public function run(): void
     {
         //
+        foreach (Student::all() as $student) {
+            Registration::create([
+                'student_id' => $student->student_id,
+                'course_id' => Course::all()->random()->getKey(),
+                'year' => fake()->dateTimeThisYear()->format('Y'),
+                'status' => 'registered'
+            ]);
+        }
     }
 }
