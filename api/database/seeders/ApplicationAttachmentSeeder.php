3ab032c341da3d8d2c01b5947704ccc6e0f3928d
ProgrammerJayT <theanthem8@gmail.com>
2023-04-22 20:52:25 +0200

    Called factory

--- api/database/seeders/ApplicationAttachmentSeeder.php
+++ api/database/seeders/ApplicationAttachmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ApplicationAttachment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,5 +14,6 @@ class ApplicationAttachmentSeeder extends Seeder
     public function run(): void
     {
         //
+        ApplicationAttachment::factory(50)->create();
     }
 }
