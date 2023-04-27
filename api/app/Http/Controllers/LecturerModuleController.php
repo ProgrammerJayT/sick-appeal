f86ab4692870324f45d45db545cbefab01f09cbc
ProgrammerJayT <theanthem8@gmail.com>
2023-04-27 15:52:27 +0200

    Created lecturer module

---
+++ api/app/Http/Controllers/LecturerModuleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LecturerModule;
+use App\Http\Requests\StoreLecturerModuleRequest;
+use App\Http\Requests\UpdateLecturerModuleRequest;
+
+class LecturerModuleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLecturerModuleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LecturerModule $lecturerModule)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LecturerModule $lecturerModule)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLecturerModuleRequest $request, LecturerModule $lecturerModule)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LecturerModule $lecturerModule)
+    {
+        //
+    }
+}
