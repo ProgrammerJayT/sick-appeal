63354e06b341c4929db25eece96feb890f3daf30
ProgrammerJayT <theanthem8@gmail.com>
2023-04-30 10:06:42 +0200

    Implemented seeder

--- api/database/seeders/RegistrationSeeder.php
+++ api/database/seeders/RegistrationSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Account;
 use App\Models\Course;
+use App\Models\Lecturer;
+use App\Models\LecturerRegistration;
 use App\Models\Registration;
 use App\Models\Student;
+use App\Models\StudentRegistration;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,12 +20,29 @@ class RegistrationSeeder extends Seeder
     public function run(): void
     {
         //
-        foreach (Student::all() as $student) {
-            Registration::create([
-                'student_id' => $student->student_id,
+        $accounts = Account::where('type', '!=', 'admin')->get();
+        $thisYear = now()->format('Y');
+        $previousYear = now()->subYear()->year;
+
+        // dd($accounts);
+        foreach ($accounts as $account) {
+            $year = fake()->randomElement(array($thisYear, $previousYear));
+
+            $registration = Registration::create([
                 'course_id' => Course::all()->random()->getKey(),
-                'year' => fake()->dateTimeThisYear()->format('Y'),
-                'status' => fake()->randomElement(array('registered', 'deregistered'))
+                'year' => $year,
+                'status' => $year == $thisYear ? 'registered' : 'deregistered'
+            ]);
+
+            $userType = $account->type;
+            $userModel = $userType == 'lecturer' ? new Lecturer : new Student;
+            $userRegistration = $userType == 'lecturer' ? new LecturerRegistration : new StudentRegistration;
+            $user = $userModel->where('account_id', $account->account_id)->first();
+            $userId = $userType == 'lecturer' ? $user->lecturer_id : $user->student_id;
+
+            $userRegistration->create([
+                $userType . '_id' => $userId,
+                'registration_id' => $registration->registration_id
             ]);
         }
     }
