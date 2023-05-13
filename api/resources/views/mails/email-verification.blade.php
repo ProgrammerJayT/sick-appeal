0da5c16db9b2de0f508cca17a8420335bbbc53d0
ProgrammerJayT <theanthem8@gmail.com>
2023-05-13 17:31:13 +0200

    Created email class and template

---
+++ api/resources/views/mails/email-verification.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Email Verification</title>
+</head>
+
+<body>
+    <h1>Email Verification</h1>
+    <p>Please click the following link to verify your email address:</p>
+    <a href="{{ route('verify-email', ['token' => $token]) }}">Verify Email</a>
+</body>
+
+</html>
