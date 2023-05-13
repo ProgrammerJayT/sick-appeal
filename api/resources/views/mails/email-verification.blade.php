bf41da6091d55551411758bea83c00d8c47c56b4
ProgrammerJayT <theanthem8@gmail.com>
2023-05-13 18:48:37 +0200

    Fixed email shit

--- api/resources/views/mails/email-verification.blade.php
+++ api/resources/views/mails/email-verification.blade.php
@@ -8,7 +8,8 @@
 <body>
     <h1>Email Verification</h1>
     <p>Please click the following link to verify your email address:</p>
-    <a href="{{ route('verify-email', ['token' => $token]) }}">Verify Email</a>
+    <p>The token is : {{ $token }}</p>
+    {{-- <a href="{{ route('verify-email', ['token' => $token]) }}">Verify Email</a> --}}
 </body>
 
 </html>
