b99faf76cf4749653f3b5143b6e6f8e0f2e486c1
ProgrammerJayT <theanthem8@gmail.com>
2023-05-14 17:45:30 +0200

    Students emails

--- api/app/Http/Controllers/SickTestController.php
+++ api/app/Http/Controllers/SickTestController.php
@@ -58,6 +58,8 @@ class SickTestController extends Controller
 
             $studentModules = StudentModule::where('module_id', $request->testId)->get();
 
+            return $studentModules;
+
             foreach ($studentModules as $studentModule) {
                 $student = Student::find($studentModule->student_id);
                 $account = Account::find($student->account_id);
