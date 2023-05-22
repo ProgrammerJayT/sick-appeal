a3146ee951fb4eac2ecfe14de4eb61b899c70a72
ProgrammerJayT <theanthem8@gmail.com>
2023-05-22 18:26:46 +0200

    Created sick test resource

---
+++ api/app/Http/Resources/SickTestResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SickTestResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return array(
+            'testId' => $this->test_id,
+            'lecturerId' => $this->lecturer_id,
+            'moduleId' => $this->module_id,
+            'date' => $this->date,
+            'time' => $this->time,
+            'type' => $this->type,
+            'venueId' => $this->venue_id,
+        );
+    }
+}
